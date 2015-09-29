7247949eef3a89a260c94074ec713c666c866d1f
thationothe <thationothe71@gmail.com>
2015-09-29 02:11:29 +0000

    Still working Ex. 7.2

--- lance/ex7.2.py
+++ lance/ex7.2.py
@@ -6,5 +6,18 @@ for line in fh:
     if not line.startswith("X-DSPAM-Confidence:") : continue
     print line
 
+count = 0
+
+for var in fh:
+    count = count + 1
+    print count
+
+# print line.find('0.')
+
+# slice = line[::27]
+# print slice
+
+#print line
+
 # avg = sum(conf_num,0.0) / len(conf_num)
     # print "The average of {0} numbers is {1}".format(len(conf_num),avg)
